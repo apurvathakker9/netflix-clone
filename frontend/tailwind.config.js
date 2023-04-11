b1ef11ffa1e243b52a7ad2261c3968eb1343bfc0
apurvathakker9 <atapurvathakker906@gmail.com>
2023-04-11 11:34:04 +0530

    Next js default setup done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
 }
